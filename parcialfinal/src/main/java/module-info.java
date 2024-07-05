f7efc63f3a4963bcab2f3816b58e852e3acb7614
cherruca <00402523@uca.edu.sv>
2024-07-05 12:21:32 -0600

    :card_file_box: conexión a base de datos

--- parcialfinal/src/main/java/module-info.java
+++ parcialfinal/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.example.parcialfinal {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens org.example.parcialfinal to javafx.fxml;
--- parcialfinal/src/main/java/org/example/parcialfinal/HelloController.java
+++ parcialfinal/src/main/java/org/example/parcialfinal/HelloController.java
@@ -1,14 +1,44 @@
 package org.example.parcialfinal;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import org.example.parcialfinal.modelo.Facilitador;
 
-public class HelloController {
+import java.net.URL;
+import java.sql.*;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
     @FXML
     private Label welcomeText;
+
+
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void conexionPrueba() {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/pooparcialfinal", "parcial", "1234");
+            Statement stmt = connection.createStatement();
+            String query = "SELECT * FROM facilitador";
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                Facilitador facilitador = new Facilitador(rs.getInt("id"), rs.getString("tipo"));
+
+                System.out.println(facilitador.getId()+"\t"+facilitador.getTipo());
+            }
+            connection.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        conexionPrueba();
     }
 }
