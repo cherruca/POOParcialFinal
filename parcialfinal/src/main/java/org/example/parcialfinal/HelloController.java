67fd7d511edfc1cab7d7228597be0f29efcd97d1
cherruca <00402523@uca.edu.sv>
2024-07-05 12:30:57 -0600

    :lipstick: vista y controlador configurados

--- parcialfinal/src/main/java/org/example/parcialfinal/HelloController.java
+++ parcialfinal/src/main/java/org/example/parcialfinal/HelloController.java
@@ -2,7 +2,8 @@ package org.example.parcialfinal;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
 import org.example.parcialfinal.modelo.Facilitador;
 
 import java.net.URL;
@@ -11,14 +12,94 @@ import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
     @FXML
-    private Label welcomeText;
-
-
+    private Button btnBuscarCliente;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
+    private Button btnBuscarCompra;
+
+    @FXML
+    private Button btnBuscarTarjeta;
+
+    @FXML
+    private Button btnEliminarCliente;
+
+    @FXML
+    private Button btnEliminarCompra;
+
+    @FXML
+    private Button btnEliminarTarjeta;
+
+    @FXML
+    private Button btnGuardarCliente;
+
+    @FXML
+    private Button btnGuardarCompra;
+
+    @FXML
+    private Button btnGuardarTarjeta;
+
+    @FXML
+    private ComboBox<?> cbClientes;
+
+    @FXML
+    private ComboBox<?> cbFacilitador;
+
+    @FXML
+    private ComboBox<?> cbTarjetas;
+
+    @FXML
+    private ComboBox<?> cbTipoTarjeta;
+
+    @FXML
+    private DatePicker datePickerFechaCompra;
+
+    @FXML
+    private TableView<?> tblClientes;
+
+    @FXML
+    private TableView<?> tblCompras;
+
+    @FXML
+    private TableView<?> tblTarjetas;
+
+    @FXML
+    private TextArea txtAreaDescripcion;
+
+    @FXML
+    private TextArea txtAreaDireccion;
+
+    @FXML
+    private TextField txtBuscarCliente;
+
+    @FXML
+    private TextField txtBuscarCompra;
+
+    @FXML
+    private TextField txtBuscarTarjeta;
+
+    @FXML
+    private TextField txtClienteApellido;
+
+    @FXML
+    private TextField txtClienteNombre;
+
+    @FXML
+    private TextField txtClienteTelefono;
+
+    @FXML
+    private TextField txtCvc;
+
+    @FXML
+    private TextField txtFechaVencimiento;
+
+    @FXML
+    private TextField txtMonto;
+
+    @FXML
+    private TextField txtNumTarjeta;
+
+    @FXML
+    private VBox vbMenu;
 
     public void conexionPrueba() {
         try {
