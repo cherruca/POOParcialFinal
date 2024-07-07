1cd06ddbd791c28eeafab7903af3895c81c65202
00191322 <00191322@uca.edu.sv>
2024-07-06 23:40:18 -0600

    :bug: Se agrego el paquete modelo
    Ahora estas excepciones/advertencias ya no se lanzan:
    
    WARNING: Can not retrieve property 'id' in PropertyValueFactory: javafx.scene.control.cell.PropertyValueFactory@22c25f3f with provided class type: class org.example.parcialfinal.modelo.Cliente
    
    Caused by: java.lang.IllegalAccessException: module javafx.base cannot access class org.example.parcialfinal.modelo.Cliente (in module org.example.parcialfinal) because module org.example.parcialfinal does not open org.example.parcialfinal.modelo to javafx.base

--- parcialfinal/src/main/java/module-info.java
+++ parcialfinal/src/main/java/module-info.java
@@ -2,8 +2,10 @@ module org.example.parcialfinal {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires java.desktop;
 
 
     opens org.example.parcialfinal to javafx.fxml;
+    opens org.example.parcialfinal.modelo to javafx.base;
     exports org.example.parcialfinal;
 }
