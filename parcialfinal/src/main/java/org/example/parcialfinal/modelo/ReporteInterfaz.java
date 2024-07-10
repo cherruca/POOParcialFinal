9f85f502a4e163a1e750c35e6f3f002b6a7e0bcd
cherruca <00402523@uca.edu.sv>
2024-07-09 23:47:34 -0600

    :memo: documentacion de reporte A

--- parcialfinal/src/main/java/org/example/parcialfinal/modelo/ReporteInterfaz.java
+++ parcialfinal/src/main/java/org/example/parcialfinal/modelo/ReporteInterfaz.java
@@ -3,12 +3,12 @@ package org.example.parcialfinal.modelo;
 import java.io.File;
 import java.sql.*;
 
-public interface ReporteInterfaz {
-    public void generarConsulta(ReporteParametro reporte);
+public interface ReporteInterfaz { // 00402523 se crea una interfaz para implementar los reportes
+    public void generarConsulta(ReporteParametro reporte); // 00402523 se declara metodo para sobreescribir
 
-    public String generarReporte(ResultSet rs);
+    public String generarReporte(ResultSet rs); // 00402523 se declara metodo para sobreescribir
 
-    public String abrirArchivo(File archivoRuta);
+    public String abrirArchivo(File archivoRuta); // 00402523 se declara metodo para sobreescribir
 
-    public void guardarArchivo(String texto);
+    public void guardarArchivo(String texto); // 00402523 se declara metodo para sobreescribir
 }
